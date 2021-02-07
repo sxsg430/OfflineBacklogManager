0fe919978c0293b2aa97464bc7815efc8880e80e
sxsg430 <872597+sxsg430@users.noreply.github.com>
2021-02-07 11:16:15 +1000

    Add initial components for SQL

---
+++ Model/SQL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace OfflineBacklogManager.Model
+{
+    public class SQL
+    {
+        public string ConnString { get; set; }
+        public SQL(string connectionStr)
+        {
+            this.ConnString = connectionStr;
+        }
+
+        private MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnString);
+        }
+    }
+}
