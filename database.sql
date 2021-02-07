4be78d537de97134169f900a3de2768c323f6887
sxsg430 <872597+sxsg430@users.noreply.github.com>
2021-02-07 11:06:42 +1000

    Add Initial SQL file

---
+++ database.sql
@@ -0,0 +1,4 @@
+﻿DROP DATABASE obm;
+CREATE DATABASE obm;
+USE obm;
+CREATE TABLE obm_list (ID int NOT NULL AUTO_INCREMENT PRIMARY KEY, title text, gamesystem text, ownership int, state int, achievement int, achievement_max int, onlineinfo text, progress text, rating int, playing int, whishlist int);
