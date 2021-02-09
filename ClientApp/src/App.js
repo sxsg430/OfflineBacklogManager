2001886876e865a576ce9b834a9dc07264c8e47a
sxsg430 <872597+sxsg430@users.noreply.github.com>
2021-02-09 22:50:14 +1000

    Start creating initial new game page.

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -4,6 +4,7 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
+import { AddNew } from './components/AddNew';
 
 import './custom.css'
 
@@ -16,6 +17,7 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
+        <Route path='/add' component={AddNew} />
       </Layout>
     );
   }
---
+++ ClientApp/src/components/AddNew.js
@@ -0,0 +1,22 @@
+﻿import React, { Component } from 'react';
+import { Alert } from 'reactstrap';
+
+export class AddNew extends Component {
+    static displayName = AddNew.name;
+    constructor(props) {
+        super(props);
+
+        this.state = {}
+
+    }
+
+    render() {
+        return (
+            <main>
+                <Alert color="primary">
+                    Placeholder element.
+                </Alert>
+            </main>
+            )
+    }
+}
