b70d970e24d032b11ecdf2c910198f91c1e83023
sxsg430 <872597+sxsg430@users.noreply.github.com>
2021-02-09 23:36:38 +1000

    Add reusable dropdown for gameplay status

--- ClientApp/src/components/AddNew.js
+++ ClientApp/src/components/AddNew.js
@@ -1,13 +1,22 @@
 ﻿import React, { Component } from 'react';
-import { Alert } from 'reactstrap';
+import { Alert, Input, ButtonGroup } from 'reactstrap';
+import StatusDropdown from './ui_components/StatusDropdown';
 
 export class AddNew extends Component {
     static displayName = AddNew.name;
     constructor(props) {
         super(props);
+        this.updateStatus = this.updateStatus.bind(this);
 
-        this.state = {}
+        this.state = {
+            currentStatus: 'Status'
+        }
 
+    }
+
+    updateStatus = (value) => {
+        let internalVal = value;
+        this.setState({ currentStatus: internalVal });
     }
 
     render() {
@@ -16,6 +25,9 @@ export class AddNew extends Component {
                 <Alert color="primary">
                     Placeholder element.
                 </Alert>
+                <ButtonGroup>
+                    <StatusDropdown updateState={this.updateStatus}/>
+                </ButtonGroup>
             </main>
             )
     }
---
+++ ClientApp/src/components/ui_components/StatusDropdown.js
@@ -0,0 +1,53 @@
+﻿import React, { Component } from 'react';
+import { DropdownToggle, DropdownMenu, DropdownItem, ButtonDropdown } from 'reactstrap';
+
+class StatusDropdown extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            Open: false,
+            currValue: "Status",
+        }
+    }
+
+    toggle = () => {
+        this.setState({Open: !this.state.Open})
+    }
+
+    updateSelection = (val) => {
+        // Get Inner Text of the element provided and update local and parent state.
+        this.setState({ currValue: val.currentTarget.innerText });
+        this.props.updateState(val.currentTarget.innerText);
+    }
+
+    textValue = () => {
+        // If the dropdown is loaded from the edit page, show the default value from the parent (the existing status from the game being edited). Otherwise, use the internal default value.
+        if (this.props.edit === 1) {
+            return this.props.currentValue;
+        } else {
+            return this.state.currValue;
+        }
+    }
+
+    render() {
+        return (
+            <ButtonDropdown direction="down" isOpen={this.state.Open} toggle={this.toggle}>
+                <DropdownToggle className="menu-button" caret color="primary">
+                    {this.textValue()}
+                </DropdownToggle>
+                <DropdownMenu>
+                    <DropdownItem style={{ fontWeight: 500, color: 'black' }} onClick={this.updateSelection}>Unplayed</DropdownItem>
+                    <DropdownItem style={{ fontWeight: 500, color: 'black' }} onClick={this.updateSelection}>Incomplete</DropdownItem>
+                    <DropdownItem style={{ fontWeight: 500, color: 'black' }} onClick={this.updateSelection}>Beaten</DropdownItem>
+                    <DropdownItem style={{ fontWeight: 500, color: 'black' }} onClick={this.updateSelection}>Complete</DropdownItem>
+                    <DropdownItem style={{ fontWeight: 500, color: 'black' }} onClick={this.updateSelection}>Mastered</DropdownItem>
+                    <DropdownItem style={{ fontWeight: 500, color: 'black' }} onClick={this.updateSelection}>N/A</DropdownItem>
+                </DropdownMenu>
+            </ButtonDropdown>
+            )
+    }
+}
+
+export default StatusDropdown;
