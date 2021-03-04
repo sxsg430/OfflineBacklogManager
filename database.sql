01e713f8f31c73f3e20eadcba5c40c35d63879e6
sxsg430 <872597+sxsg430@users.noreply.github.com>
2021-03-04 12:41:20 +1000

    Add SQL for partially complete add form

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
 ﻿DROP DATABASE obm;
 CREATE DATABASE obm;
 USE obm;
-CREATE TABLE obm_list (ID int NOT NULL AUTO_INCREMENT PRIMARY KEY, title text, gamesystem text, ownership int, status int, achievement int, achievement_max int, progress text, playing int, whishlist int, appid text);
+CREATE TABLE obm_list (ID int NOT NULL AUTO_INCREMENT PRIMARY KEY, title text, gamesystem text, ownership text, status text, achievement text, achievement_max text, progress text, playing text, wishlist text, appid text);
