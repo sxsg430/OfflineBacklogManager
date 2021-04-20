ef8be1300ca26a665b1ef40d011ce824e953e1f1
sxsg430 <872597+sxsg430@users.noreply.github.com>
2021-04-21 09:26:32 +1000

    Add initial code for list and search endpoint

---
+++ Controllers/ListGameController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using OfflineBacklogManager.Models;
+using OfflineBacklogManager.DBContexts;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace OfflineBacklogManager.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ListGameController : ControllerBase
+    {
+        private OBMDBContext OBContext;
+        public ListGameController(OBMDBContext context)
+        {
+            OBContext = context;
+        }
+
+        [HttpGet]
+        public Models.Game[] Get()
+        {
+            // TODO: Add Search functions
+            var local = OBContext.Games.ToArray();
+            return local;
+        }
+    }
+}
